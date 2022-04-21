47c63690e25ec5ab7a0d5f19ae3a536e18e0194d
YASHVARDHAN-DEV <80517622+YASHVARDHAN-DEV@users.noreply.github.com>
2022-04-21 17:07:46 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyAI4eeGYC2uWRGKBsfC5xHnls86quUPA_U",
+    authDomain: "spectagram-32301.firebaseapp.com",
+    projectId: "spectagram-32301",
+    storageBucket: "spectagram-32301.appspot.com",
+    messagingSenderId: "970390523090",
+    appId: "1:970390523090:web:cfda9a5cc594c323d8710b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
